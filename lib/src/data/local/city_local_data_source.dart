5497df18f69e74f080f1fbcd029691040f667ee9
Saleh <salehiscoding@gmail.com>
2024-01-06 22:33:57 +0330

    Building local repositories

---
+++ lib/src/data/local/city_local_data_source.dart
@@ -0,0 +1,40 @@
+import 'package:hive/hive.dart';
+import 'package:weather_forecast_app_zarinpal/src/data/models/city_model.dart';
+import 'package:weather_forecast_app_zarinpal/src/utils/di.dart';
+
+abstract class ICityLocalDataSource {
+  Future<List<City>> getSelectedCities();
+  Future<void> saveSelectedCity(City city);
+  Future<void> removeSelectedCity(City city);
+}
+
+class CityLocalDataSource implements ICityLocalDataSource {
+  final Box<City> _citiesBox = locator<Box<City>>();
+
+  @override
+  Future<List<City>> getSelectedCities() async {
+    try {
+      return _citiesBox.values.toList();
+    } on Exception catch (ex) {
+      throw ex;
+    }
+  }
+
+  @override
+  Future<void> saveSelectedCity(City city) async {
+    try {
+      await _citiesBox.add(city);
+    } on Exception catch (ex) {
+      throw ex;
+    }
+  }
+
+  @override
+  Future<void> removeSelectedCity(City city) async {
+    try {
+      await _citiesBox.delete(city.key);
+    } on Exception catch (ex) {
+      throw ex;
+    }
+  }
+}
