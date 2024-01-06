75a788921e4a77425a26af74e80944e8182f6f7c
Saleh <salehiscoding@gmail.com>
2024-01-07 02:32:19 +0330

    Creating a cached widget called cached_icon using CachedNetworkImage for better performance

---
+++ lib/src/widgets/cached_icon.dart
@@ -0,0 +1,37 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_forecast_app_zarinpal/src/constants/const.dart';
+
+class CachedIcon extends StatelessWidget {
+  final String? iconId;
+  final double radius;
+  const CachedIcon({super.key, this.iconId, this.radius = 0});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(radius),
+      child: CachedNetworkImage(
+        fit: BoxFit.cover,
+        imageUrl: "${APIConfig.IconUrlPrefix}${iconId ?? '01d'}${APIConfig.IconUrlSuffix}",
+        placeholder: (context, url) {
+          return Container(
+            decoration: BoxDecoration(
+              gradient: ColorPack.darkPurpleGradient,
+            ),
+          );
+        },
+        errorWidget: (context, url, error) => Container(
+          width: 20,
+          height: 20,
+          decoration: BoxDecoration(
+            gradient: ColorPack.darkPurpleGradient,
+          ),
+          child: Center(
+            child: Text("❌"),
+          ),
+        ),
+      ),
+    );
+  }
+}
